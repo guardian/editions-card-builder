6fd9bb6f44d23f606fa0bf099386821af18223c3
Sam Hession <samhession@live.co.uk>
2020-07-06 17:03:18 +0100

    Adds debounce to canvas draw

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -3,6 +3,7 @@ import { jsx } from '@emotion/core'
 import { Furniture } from '../types/furniture'
 import * as React from 'react'
 import CanvasCard from '../utils/canvas'
+import debounce from 'debounce'
 
 interface CanvasProps {
   update: (blob : Blob) => void
@@ -11,14 +12,20 @@ interface CanvasProps {
 
 interface CanvasState {
   card: CanvasCard
+  drawDebounce: any
 }
 
 class Canvas extends React.Component<CanvasProps, CanvasState> {
   constructor(props){
     super(props)
     this.state = {
-      card: new CanvasCard()
+      card: new CanvasCard(),
+      drawDebounce: undefined
     }
+  }
+
+  componentDidMount(){
+    this.setState({drawDebounce : debounce(this.draw, 200)});
   }
 
   shouldComponentUpdate(nextProps){
@@ -27,10 +34,15 @@ class Canvas extends React.Component<CanvasProps, CanvasState> {
 
   componentDidUpdate() {
     const canvas = this.refs.canvas as HTMLCanvasElement;
+    this.state.drawDebounce.clear();
+    this.state.drawDebounce(canvas, this.state, this.props)
 
-    this.state.card.draw(canvas, this.props.furniture)
-      .then(() => canvas.toBlob( (blob) => this.props.update(blob)));
+  }
 
+  draw(canvas: HTMLCanvasElement, state: CanvasState, props: CanvasProps) {
+    console.log("draw");
+    state.card.draw(canvas, props.furniture)
+      .then(() => canvas.toBlob( (blob) => props.update(blob)));
   }
 
   render() {
