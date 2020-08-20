a3c8d23d7ee5f0c4e1a0760ff07d930493d39c6e
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-08-20 11:39:56 +0100

    Revert "Increase height of mobile cards to 1.8x width"

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -14,7 +14,7 @@ const standfirstLineHeightMultiplier = 1.1;
 export default {
   gridDomain: process.env.GRID_DOMAIN as string,
   dimensions: {
-    mobile: [525, 945], // This is the ratio of an iPhone 5S, the narrowest screen we tolerate
+    mobile: [525, 810],
     tablet: [975, 1088]
   },
   padding: 10,
