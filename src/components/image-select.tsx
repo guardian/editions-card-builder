b8facdf8ef91ba87220b3bfeeaa8a40ba09edee8
Akash Askoolum <akash1810@gmail.com>
2020-09-29 20:34:58 +0100

    use grid-client for Grid crop validation

--- src/components/image-select.tsx
+++ src/components/image-select.tsx
@@ -3,6 +3,8 @@ import { jsx } from '@emotion/core'
 import * as React from 'react';
 import config from "../utils/config"
 import Modal from './modal';
+import {IframePostMessageService} from "@guardian/grid-client"
+import {Reporter} from "@guardian/grid-client/lib/utils"
 
 interface GridModalProps {
   updateImageUrl: (imageUrl: string) => void
@@ -50,23 +52,20 @@ class ImageSelect extends React.Component<GridModalProps, GridModalState> {
       return;
     }
 
-    const data = event.data;
+    const postMessageService = new IframePostMessageService(event, Reporter.default)
 
-    if (!data) {
+    if(!postMessageService.isValid) {
       return;
     }
 
-    if (!this.validMessage(data)) {
-      return;
-    }
+    const imageUrl: URL = postMessageService.highestQualityImageURL!;
 
-    const imageUrl = event.data.crop.data.master.secureUrl;
     this.setState({
-      imageId: event.data.image.data.id as string
+      imageId: postMessageService.imageId!
     });
 
     this.closeModal();
-    this.props.updateImageUrl(imageUrl);
+    this.props.updateImageUrl(imageUrl.toString());
     this.props.updateOriginalImageData(event.data.image);
   };
 
